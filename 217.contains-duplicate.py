22950686a75cc097b6dd62f86b29d6fb24de20ab
Nitin Mohan <mnitin31@gmail.com>
2026-03-01 00:50:16 +0000

    solve: 217 with hashset

--- 217.contains-duplicate.py
+++ 217.contains-duplicate.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for ele in nums:
+            if ele in hashset:
+                return True
+            hashset.add(ele)
+        return False
+
         
 # @lc code=end
 
